7e0ef95093b497df7152832911e59430d6f418a9
ChetanSingh15 <chetanrabary@gmail.com>
2025-06-28 12:03:10 +0530

    Project Dashboard - Circle-Progress, Navbar

--- app/(protected)/workspace/[workspaceId]/layout.tsx
+++ app/(protected)/workspace/[workspaceId]/layout.tsx
@@ -1,5 +1,6 @@
 
 import { getUserWorkspaces } from '@/app/data/workspace/get-user-workspaces';
+import { Navbar } from '@/components/navbar';
 import { AppSidebarContainer } from '@/components/sidebar/app-sidebar-container';
 import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
 import { redirect } from 'next/navigation';
@@ -27,12 +28,12 @@ const WorkspaceIdLayout = async ({children,params}: Props) => {
         <main className='w-full overflow-y-auto min-h-screen'>
             <div className='flex items-start'>
                 <SidebarTrigger className='pt-3'/>
-                {/* <Navbar
+                <Navbar
                 id = {data?.id} 
                 name = {data?.name as string}
                 email = {data?.email as string} 
                 image = {data?.image as string} 
-                /> */}
+                />
             </div>
             <div className='p-0 md:p-4 pt-2'>
                 {children}
--- components/project/project-dashboard.tsx
+++ components/project/project-dashboard.tsx
@@ -5,6 +5,7 @@ import { Card } from "../ui/card";
 import { TaskDistributionChart } from "./task-distribution-chart";
 import { ActivityFeed } from "./activity-feed";
 import { CommentList } from "./comment-list";
+import { CircleProgress } from "./circle-progress";
 
 
 interface ProjectDashboardProps {
@@ -29,25 +30,55 @@ export const ProjectDashboard = ({
     comments
 }: ProjectDashboardProps) => {
     return <>
-    <div className="flex flex-col gap-6 px-2 md:px-4 2xl:px-6 py-0">
-        <ProjectHeader project={project as unknown as ProjectProps} />
+        <div className="flex flex-col gap-6 px-2 md:px-4 2xl:px-6 py-0">
+            <ProjectHeader project={project as unknown as ProjectProps} />
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
-            {/* TODO: circle progress */}
-            <Card></Card>
-        </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3">
+                <Card className="pt-2 pb-2">
+                    <CircleProgress
+                        title="Task Completed"
+                        value={(tasks.completed / tasks.total) * 100}
+                        subTitle={`${tasks.completed}/${tasks.total} tasks`}
+                        variant="success"
+                    />
+                </Card>
+                <Card className="pt-2 pb-2">
+                    <CircleProgress
+                        title="Inprogress"
+                        value={(tasks.inProgress / tasks.total) * 100}
+                        subTitle={`${tasks.inProgress} tasks ongoing`}
+                        variant="inProgress"
+                    />
+                </Card>
+                <Card className="pt-2 pb-2">
+                    <CircleProgress
+                        title="Overdue"
+                        value={(tasks.overdue / tasks.total) * 100}
+                        subTitle={`${tasks.overdue} overdue tasks`}
+                        variant="warning"
+                    />
+                </Card>
+                <Card className="pt-2 pb-2">
+                    <CircleProgress
+                        title="Team Members"
+                        value={(project.members.length)}
+                        subTitle={`${project.members.length} team members`}
+                        variant="default"
+                    />
+                </Card>
+            </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            <TaskDistributionChart tasks={tasks} />
-             <Card>
-                <h3 className="text-lg font-semibold mb-4 mx-2">Recent Activities</h3>
-                <ActivityFeed activities={activities.slice(0,5) as any } />
-            </Card>
-            <Card>
-                <h3 className="text-lg font-semibold mb-4 mx-2">Recent Comments</h3>
-                <CommentList comments={comments.slice(0,5) as any} />
-            </Card>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                <TaskDistributionChart tasks={tasks} />
+                <Card className="p-2">
+                    <h3 className="text-lg font-semibold mb-2 mx-2">Recent Activities</h3>
+                    <ActivityFeed activities={activities.slice(0, 5) as any} />
+                </Card>
+                <Card className="p-2">
+                    <h3 className="text-lg font-semibold mb-2 mx-2">Recent Comments</h3>
+                    <CommentList comments={comments.slice(0, 5) as any} />
+                </Card>
+            </div>
         </div>
-    </div>
     </>
 }
